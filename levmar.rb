5ebb05557787b3e6d8f72b472a1622dc7b19e58e
Alejandro Álvarez Ayllón <a.alvarezayllon@gmail.com>
2018-10-19 09:57:04 +0200

    levmar depends on lapack

--- levmar.rb
+++ levmar.rb
@@ -5,6 +5,7 @@ class Levmar < Formula
   url "https://src.fedoraproject.org/repo/pkgs/rpms/levmar/levmar-2.5.tgz/md5/7ca14d79eda6e985f8355b719ae47d35/levmar-2.5.tgz"
   version "2.5"
   sha256 "b70f6ac3eff30ec29150e217b137312cb84e85529815efea2c12e4eab74b9d75"
+  depends_on "lapack"
 
   def install
     inreplace "Makefile.so", "-lf2c", ""
